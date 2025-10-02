dcb24524d885b5e9765222e526ee77d70bd424e5
just_alish <139762778+just-alish@users.noreply.github.com>
2025-10-02 11:56:52 +0500

    Deploy updated site

--- .history/src/utils/scrollTop_20250617181819.ts
+++
@@ -1,6 +0,0 @@
-export function topFunction() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-}
--- src/components/bg.ts
+++
@@ -1,7 +0,0 @@
-import '..styles/bg.css';
-
-export function createBg() {
-  const bg = document.createElement('p')
-  bg.className = 'bg';
-  return bg;
-}
